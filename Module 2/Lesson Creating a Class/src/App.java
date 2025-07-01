1c51c79065733ae1d967dac89bc5c40630b1f4dd
stokesm5 <stokesm5@tcnj.edu>
2025-06-30 21:51:25 -0400

    Lesson: Creating a Class

--- Module 2/Lesson Creating a Class/src/App.java
+++ Module 2/Lesson Creating a Class/src/App.java
@@ -2,14 +2,49 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // 1. instantiation
+        NationalForest one = new NationalForest("Dixie", "Utah", 123);
+        //2. set a 'public' field
+        //one.name = "Allegheny";
+        //one.location = "Pennsylvania";
+        //one.acres = 513655;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //NationalForest two = new NationalForest("Angeles", "California", 661565);
+        NationalForest two = new NationalForest("Angeles", 661565);
+        //two.name = "Angeles";
+
+        //NationalForest three = new NationalForest("Angelina", "Texas", 1554140);
+        NationalForest three = new NationalForest("Angelina");
+        //three.name = "Angelina";
+
+        NationalForest four = new NationalForest();
+
+        String format = "name: %s, location: %s, acres: %s%n";
+        // 3. read each 'public' field
+        // each object's fields have different values````````````
+        //System.out.printf(format, one.name, one.location, one.acres);
+        //System.out.printf(format, two.name, two. location, two.acres);
+        //System.out.printf(format, three.name, three.location, three.acres);
+        //System.out.printf(format, four.name, four.location, four.acres);
+
+        System.out.println(one.toLine());
+        System.out.println(two.toLine());
+        System.out.println(three.toLine());
+        System.out.println(four.toLine());
+
+        //individual fields and calculations
+        System.out.println("Forest Name: " + one.getName());
+        System.out.println("Location: " + one.getLocation());
+        System.out.println("Acres: " + one.getAcres());
+        System.out.println("Square km: " + one.getSquareKilometers());
+
+        //all together
+        System.out.println(one.toLine());
+
+        //acres is wrong, fix it
+        one.setAcres(1885655);
+        System.out.println("Fixed ---");
+        System.out.println("Acres: " + one.getAcres());
+        System.out.println("Square km: " + one.getSquareKilometers());`
     }
 }
--- Module 2/Lesson Creating a Class/src/NationalForest.java
+++ Module 2/Lesson Creating a Class/src/NationalForest.java
@@ -1,2 +1,52 @@
 public class NationalForest {
+    private String name;
+    private String location;
+    private int acres;
+
+    //Getters
+    public String getName(){
+        return name;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public int getAcres(){
+        return acres;
+    }
+
+    //setters
+    public void setAcres(int acres){
+        this.acres = (acres > 0) ? acres : 0;
+    }
+
+    //constructors
+    public NationalForest() {
+        this("Unknown", "Unknown", -1);
+    }
+
+    public NationalForest(String name){
+        this(name, "Unknown", -1);
+    }
+
+    public NationalForest(String name, int acres){
+        this(name, "Unknown", acres);
+    }
+
+    //other constructors are referencing this constructor with this(arg,arg,arg)
+    public NationalForest(String name, String location, int acres) {
+        this.name = name;
+        this.location = location;
+        this.acres = acres;
+    }
+
+    public int getSquareKilometers() {
+        return (int) (this.acres/247.1);
+    }
+
+    //returns format string
+    public String toLine(){
+        return String.format("name: %s, location: %s, acres: %s, km^2: %s", name, location, acres, getSquareKilometers());
+    }
 }
